5e3ac17e6c7f1b92a82b31ce5f257f88a42e46e1
Diego <c4p9jyqcsm@privaterelay.appleid.com>
2026-04-12 20:35:05 -0600

    feat: 2FA integration to Module Login, Flask-Mail

--- config.py
+++ config.py
@@ -12,3 +12,9 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Gerente:Moster&EatsDUCA$@localhost/monster_eats_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'monstereatsmail@gmail.com'
+    MAIL_PASSWORD = 'xtuyovvivveypura'
+
